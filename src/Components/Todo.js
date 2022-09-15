98db1136453cf1a8663c63c2242a695a25c1930e
Rashid Alam <alamm2412@gmail.com>
2022-09-16 02:26:44 +0530

    :arrow_up:react started

---
+++ src/Components/Todo.js
@@ -0,0 +1,33 @@
+import React,{Component} from 'react'
+
+export default class Todo extends Component {
+    constructor(){
+        super();
+        this.state={
+            tasks:[
+                {id:1, task:"Revise JS"},
+                {id:2, task:"Revise DSA level-1"},
+            ]
+        };
+    }
+    render(){
+        return(
+            // <div>Todo</div>
+            <div>
+                <input type="text"></input>
+                <button>Submit</button>
+                {// use when need to write js in jsx
+                this.state.tasks.map(function(taskObj){
+                    return(
+                        <li>
+                            <p>{taskObj.task}</p>
+                            <button>Submit</button>
+                        </li>
+                    );
+                 })
+
+                }
+            </div>
+        )
+    }
+}
